fcedaabee88fa5512300c23fad19d93b730fd70d
ZZWsdf <1097798399@qq.com>
2024-09-06 14:33:43 +0800

    新增文章

---
+++ src/main/java/com/zzw/service/ArticleService.java
@@ -0,0 +1,7 @@
+package com.zzw.service;
+
+import com.zzw.pojo.Article;
+
+public interface ArticleService {
+    void add(Article article);
+}
---
+++ src/main/java/com/zzw/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,27 @@
+package com.zzw.service.impl;
+
+import com.zzw.mapper.ArticleMapper;
+import com.zzw.pojo.Article;
+import com.zzw.service.ArticleService;
+import com.zzw.util.ThreadLocalUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+    @Autowired
+    private ArticleMapper articleMapper;
+    @Override
+    public void add(Article article) {
+        //补充信息
+        article.setCreateTime(LocalDateTime.now());
+        article.setUpdateTime(LocalDateTime.now());
+        Map<String,Object> map=ThreadLocalUtil.get();
+        Integer userid= (Integer) map.get("id");
+        article.setCreateUser(userid);
+        articleMapper.add(article);
+    }
+}
